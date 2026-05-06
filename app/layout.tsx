2a617b9e8b821547d3243ecf88719145322a43d1
Dao Duck <dduccraft@gmail.com>
2026-05-06 08:40:07 +0700

    fix: Remove Unused fonts and set Vietnamese Inter as default font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
 import {
-  Geist_Mono,
   Inter,
-  Playfair_Display,
   Cormorant_Garamond,
-  Outfit,
 } from "next/font/google";
 import "@/styles/global.css";
 import { ToastContainer } from "react-toastify";
@@ -12,19 +9,9 @@ import AuthHydrator from "@/components/providers/authHydrator";
 import ScrollToTop from "@/components/ui/ScrollToTop";
 import GoogleAuthProvider from "@/components/providers/gooleAuthProvider";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const inter = Inter({
   variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const playfair = Playfair_Display({
-  variable: "--font-playfair",
-  subsets: ["latin"],
+  subsets: ["latin", "vietnamese"],
 });
 
 const cormorant = Cormorant_Garamond({
@@ -32,11 +19,6 @@ const cormorant = Cormorant_Garamond({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   style: ["normal", "italic"],
-});
-
-const outfit = Outfit({
-  variable: "--font-outfit",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -52,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="vi" suppressHydrationWarning>
       <body
-        className={`${playfair.variable} ${cormorant.variable} ${outfit.variable} ${inter.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${cormorant.variable} antialiased`}
       >
         <AuthHydrator>
           <GoogleAuthProvider>
